fd766dad2d6cbd0199812d0552f878fc7878bc43
Prathamwin23 <prathamp2301@gmail.com>
2025-07-30 00:06:21 +0530

    Update apps.py

--- operations/apps.py
+++ operations/apps.py
@@ -1,1 +1,10 @@
-# apps.py placeholder
+from django.apps import AppConfig
+
+class OperationsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'operations'
+    verbose_name = 'Field Operations'
+
+    def ready(self):
+        # Import signals if needed
+        pass
